2ea95ba0661c25a28795fbe5bdf023c3e8083f4d
Yottaphy <jorge13romero@gmail.com>
2023-10-05 14:27:47 +0100

    transmission plots done for 100 and 2k ions

--- projectionplotter.py
+++ projectionplotter.py
@@ -1,71 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scienceplots
+
+plt.style.use("science")
 import matplotlib.gridspec as gridspec
+
 
 def avg(listin):
     tot = 0
-    n   = 0
+    n = 0
     for i in range(len(listin)):
         if listin[i] != 0:
-            n   += 1
+            n += 1
             tot += listin[i]
-    return tot/n
+    return tot / n
 
 
 def column(matrix, i):
     return [row[i] for row in matrix]
 
-fileHe = np.loadtxt("Optimal/NewGeometry/widerange_notopt.txt" , dtype='f')
-fileVac = np.loadtxt("Optimal/NewGeometry/newgeo_vacuum1k.txt" , dtype='f')
+
+fileHe = np.loadtxt("Optimal/NewGeometry/widerange_notopt.txt", dtype="f")
+fileVac = np.loadtxt("Optimal/NewGeometry/newgeo_vacuum1k.txt", dtype="f")
 # fileAr = np.loadtxt("Optimal/NewGeometry/widerange_notopt.txt" , dtype='f')
 
 nrows = 21
 ncols = 15
 col30 = 7
 
-He = np.zeros((nrows,ncols))
-Vac = np.zeros((nrows,ncols))
-Ar = np.zeros((nrows,ncols))
+He = np.zeros((nrows, ncols))
+Vac = np.zeros((nrows, ncols))
+Ar = np.zeros((nrows, ncols))
 brfq = []
 srfq = list(fileHe[0])
 srfq.pop(0)
 
-for i in range(1,nrows+1):
+for i in range(1, nrows + 1):
     brfq.append(fileHe[i][0])
-    for j in range(1,ncols+1):
-        He[i-1][ncols-j] = fileHe[i][j]
-        Vac[i-1][ncols-j] = fileVac[i][j]
+    for j in range(1, ncols + 1):
+        He[i - 1][ncols - j] = fileHe[i][j]
+        Vac[i - 1][ncols - j] = fileVac[i][j]
         # Ar[i-1][ncols-j] = fileAr[i][j]
 
-#Plot only column for which SRFQ=30 V
+# Plot only column for which SRFQ=30 V
 plotHe = []
 plotVac = []
 plotOther = []
-#plotAr = []
+# plotAr = []
 
-for i in range(0,nrows):
-    plotHe.append(He[i][col30-1])
-    plotVac.append(Vac[i][col30-1])
+for i in range(0, nrows):
+    plotHe.append(He[i][col30 - 1])
+    plotVac.append(Vac[i][col30 - 1])
     # plotOther.append(Vac[i][-1])
-    #plotAr.append(Ar[i][col30-1])
+    # plotAr.append(Ar[i][col30-1])
 
 print(plotVac)
 
-plt.rcParams['font.size'] = 18
+plt.rcParams["font.size"] = 18
 
 fig, axproj = plt.subplots()
-axproj.plot(brfq, plotVac, color='Green', label = "Vacuum")
-axproj.plot(brfq, plotHe, color='Red', label = "Helium")
+axproj.plot(brfq, plotVac, color="Green", label="Vacuum")
+axproj.plot(brfq, plotHe, color="Red", label="Helium")
 # axproj.plot(brfq, plotOther, color='Orange', label = "Vacuum @ 70 V SRFQ")
 # axproj.plot(brfq, plotAr, color='Blue', label = "Argon")
-axproj.set_xlim(0,410)
-axproj.set_ylim(0,100)
+axproj.set_xlim(0, 410)
+axproj.set_ylim(0, 100)
 axproj.set_aspect(2.2)
-axproj.tick_params(top=True, right=True,direction='in')
+axproj.tick_params(top=True, right=True, direction="in")
 axproj.set_title("Straight RFQ Voltage Amplitude 30V")
 axproj.set_xlabel("Bend RFQ Voltage Amplitude [V]")
 axproj.set_ylabel("Transmission [%]")
-fig.legend(fontsize='x-small', loc=1, frameon=True,fancybox=True,framealpha=1)
+fig.legend(fontsize="x-small", loc=1, frameon=True, fancybox=True, framealpha=1)
 # axproj.set_title("Transmission at 30 V Straight RFQ Voltage")
-plt.savefig("projection.pdf", bbox_inches = 'tight', pad_inches = 0.1, transparent=True)
-plt.savefig("projection.png", bbox_inches = 'tight', pad_inches = 0.1)
+plt.savefig(
+    "projection_science.pdf", bbox_inches="tight", pad_inches=0.1, transparent=True
+)
--- transmissionplotter.py
+++ transmissionplotter.py
@@ -1,43 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import matplotlib.colors as col
+import scienceplots
+
+plt.style.use("science")
+
 
 def avg(listin):
     tot = 0
-    n   = 0
+    n = 0
     for i in range(len(listin)):
         if listin[i] != 0:
-            n   += 1
+            n += 1
             tot += listin[i]
-    return tot/n
+    return tot / n
 
 
 def column(matrix, i):
     return [row[i] for row in matrix]
 
-filename = 'Optimal/NewGeometry/newgeo_argon_opt'
 
-filein = np.loadtxt(filename+".txt" , dtype='f')
+filename = "Optimal/Hechamfer2"
 
-thecolor = 'Blue'
-nrows = 23
-ncols = 23
+nrows = 20
+ncols = 20
 
-z = np.zeros((nrows,ncols))
+
+if "Ar" in filename:
+    thecolor = "Red"
+elif "He" in filename:
+    thecolor = "Blue"
+else:
+    thecolor = "Green"
+
+filein = np.loadtxt(filename + ".txt", dtype="f")
+
+z = np.zeros((nrows, ncols))
 brfq = []
 srfq = list(filein[0])
 srfq.pop(0)
 
-for i in range(1,nrows+1):
+for i in range(1, nrows + 1):
     brfq.append(filein[i][0])
-    for j in range(1,ncols+1):
-        z[i-1][ncols-j] = filein[i][j]
+    for j in range(1, ncols + 1):
+        z[i - 1][ncols - j] = filein[i][j]
 
 colmax = []
 rowmax = []
-for i in range(0,max(nrows,ncols)):
+for i in range(0, max(nrows, ncols)):
     if i < ncols:
-        colmax.append(max(column(z,i)))
+        colmax.append(max(column(z, i)))
     if i < nrows:
         rowmax.append(max(list(z[i])))
 colmax = colmax[::-1]
@@ -46,7 +59,7 @@ colmax = colmax[::-1]
 # for i in range(0,ncols):
 #     colavg.append(avg(column(z,i)))
 
-plt.rcParams['font.size'] = 18
+plt.rcParams["font.size"] = 18
 
 fig = plt.figure(figsize=(20, 15))
 gs = gridspec.GridSpec(10, 12)
@@ -57,24 +70,30 @@ axtop = plt.subplot(gs[4:5, 5:10])
 axtop.set_visible(False)
 
 axy.plot(rowmax, brfq, color=thecolor)
-axy.set_ylim(min(brfq),max(brfq))
-axy.set_xlim(0,105)
+axy.set_ylim(min(brfq), max(brfq))
+axy.set_xlim(-15, 105)
 axy.yaxis.set_ticklabels([])
 
 axx.plot(srfq, colmax, color=thecolor)
-axx.set_xlim(min(srfq),max(srfq))
+axx.set_xlim(min(srfq), max(srfq))
+axx.set_ylim(0, 100)
 axx.xaxis.set_ticklabels([])
 
-az = ax0.imshow(z, extent=[min(srfq), max(srfq), min(brfq),max(brfq)], aspect='auto', cmap = thecolor+'s')
+az = ax0.imshow(
+    z,
+    extent=[min(srfq), max(srfq), min(brfq), max(brfq)],
+    norm=col.Normalize(vmin=0, vmax=100),
+    aspect="auto",
+    cmap=thecolor + "s",
+)
 # az = ax0.contourf(z, extent=[min(brfq), max(brfq), max(srfq),  min(srfq)], cmap = 'Blues', origin = "lower")
 # ax0.contour(z, extent=[min(brfq), max(brfq), max(srfq),  min(srfq)], colors = 'k', origin = "lower", linewidths=0.4)
 
 ax0.set_xlabel("Straight RFQ Voltage Amplitude [V]")
 ax0.set_ylabel("Bend RFQ Voltage Amplitude [V]")
-clb = fig.colorbar(az, ax=axtop, orientation = 'horizontal', aspect = 50)
-clb.set_label("Transmission [%]", loc = 'center')
-clb.ax.xaxis.set_ticks_position('top')
-clb.ax.xaxis.set_label_position('top')
+clb = fig.colorbar(az, ax=axtop, orientation="horizontal", aspect=50)
+clb.set_label("Transmission [\%]", loc="center")
+clb.ax.xaxis.set_ticks_position("top")
+clb.ax.xaxis.set_label_position("top")
 
-plt.savefig(filename+".pdf", bbox_inches = 'tight', pad_inches = 0.1, transparent=True)
-plt.savefig(filename+".png", bbox_inches = 'tight', pad_inches = 0.1)
+plt.savefig(filename + ".pdf", bbox_inches="tight", pad_inches=0.1, transparent=True)
